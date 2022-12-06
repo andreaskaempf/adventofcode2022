cd6cd819c803a3075cf8f16fbd8f013c55337bc5
Andreas Kaempf <andreas.kaempf@soundience.com>
2022-12-06 15:07:51 +0100

    Day 06, further optimized to use pointer to same string, rather than a copy of each subsegment

--- day06/main.go
+++ day06/main.go
@@ -24,18 +24,19 @@ func main() {
 	// using a "channel" to allow concurrent processes to return results
 	// when ready (code will continue when both results are sent back)
 	c := make(chan int)       // create the channel
-	go marker(msg, 4, c)      // run the 4-char check in background
-	go marker(msg, 14, c)     // same for 14-char check
+	go marker(&msg, 4, c)     // run the 4-char check in background
+	go marker(&msg, 14, c)    // same for 14-char check
 	part1, part2 := <-c, <-c  // collect results when ready
 	fmt.Println(part1, part2) // show results (may be out of order!)
 }
 
 // Find the position of the "marker", i.e., a block where n chars
 // are all different, send result to channel
-func marker(s string, n int, c chan int) {
-	for i := n - 1; i < len(s); i++ {
-		substr := s[i-n+1 : i+1]  // get n-char substring
-		if !duplicates2(substr) { // no duplicate chars?
+func marker(s *string, n int, c chan int) {
+	for i := n - 1; i < len(*s); i++ {
+		//substr := s[i-n+1 : i+1]  // get n-char substring
+		//if !duplicates2(substr) { // no duplicate chars?
+		if !duplicates3(s, i-n+1, i) { // no duplicate chars?
 			c <- i + 1 // send back position of marker
 			return
 		}
@@ -44,7 +45,7 @@ func marker(s string, n int, c chan int) {
 }
 
 // Check if a string contains any duplicate characters
-// (slower implementation, using map)
+// (first implementation, slow because using map)
 func duplicates(s string) bool {
 	chars := map[byte]int{}
 	for i := 0; i < len(s); i++ {
@@ -58,12 +59,27 @@ func duplicates(s string) bool {
 }
 
 // Check if a string contains any duplicate characters,
-// faster implementation using character comparisons inside
+// second and faster implementation using character comparisons inside
 // loops instead of map
 func duplicates2(s string) bool {
 	for i := 0; i < len(s); i++ {
 		for j := 0; j < i; j++ {
 			if s[i] == s[j] {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+// Check if a string contains any duplicate characters,
+// third and even faster version that takes a pointer to a string,
+// rather than a new substring copy each time; i and j are the indexes
+// of the characters to be searched, zero-based and inclusive
+func duplicates3(s *string, i, j int) bool {
+	for a := i; a <= j; a++ {
+		for b := i; b < a; b++ {
+			if (*s)[a] == (*s)[b] {
 				return true
 			}
 		}
