fdf1624afb755c7ee6aef404ea73378f3103c64b
Andreas Kaempf <andreas.kaempf@soundience.com>
2022-12-12 13:42:41 +0100

    Added some comments to Day 12 code

--- day12/main.go
+++ day12/main.go
@@ -18,7 +18,7 @@ import (
 // Main execution: run parts 1 and 2
 func main() {
 
-	// Read matrix from file, into a pseudo-matrix of byte rows
+	// Read file into a pseudo-matrix of byte rows
 	// fname = "sample.txt"  // uncomment one file name
 	fname := "input.txt"
 	m := [][]byte{}
@@ -26,18 +26,18 @@ func main() {
 		m = append(m, []byte(l))
 	}
 
-	// Find S and E first, adjust their altitudes
+	// Find S and E first, adjust their altitudes (otherwise won't work)
 	var S, E int
-	nr := len(m)
-	nc := len(m[0])
+	nr := len(m)    // number of rows
+	nc := len(m[0]) //number of columns
 	for ri := 0; ri < nr; ri++ {
 		for ci := 0; ci < nc; ci++ {
 			if m[ri][ci] == 'S' {
-				S = ri*nc + ci
-				m[ri][ci] = 'a'
+				S = ri*nc + ci  // this is the node number of S
+				m[ri][ci] = 'a' // replace 'S' with 'a'
 			} else if m[ri][ci] == 'E' {
-				E = ri*nc + ci
-				m[ri][ci] = 'z'
+				E = ri*nc + ci  // node number for E
+				m[ri][ci] = 'z' // change 'E' to 'z' per problem text
 			}
 		}
 	}
@@ -48,24 +48,23 @@ func main() {
 	// Here, feasible means "vertical climb" at most 1 (i.e., next cell is at
 	// most one letter higher). Always use a cost (e.g., 1), otherwise
 	// shortest path will be randomized (because there is no cost).
-	g := graph.New(nr * nc) // enough capacity for all nodes
+	g := graph.New(nr * nc) // create graph with enough capacity for all nodes
 	for ri := 0; ri < nr; ri++ {
 		for ci := 0; ci < nc; ci++ {
 
 			// This starting node
-			thisNode := ri*nc + ci
-			thisLetter := int(m[ri][ci])
+			thisNode := ri*nc + ci       // the node number
+			thisLetter := int(m[ri][ci]) // letter in this cell
 			assert(thisLetter >= 'a' && thisLetter <= 'z', "Bad letter")
 
 			// Go right (and left from there) if possible
-			maxDiff := 1
-			if ci < nc-1 {
-				nextNode := ri*nc + (ci + 1)
-				nextLetter := int(m[ri][ci+1])
-				if nextLetter-thisLetter <= maxDiff { // go right
-					g.AddCost(thisNode, nextNode, 1)
+			if ci < nc-1 { // all but last column
+				nextNode := ri*nc + (ci + 1)    // node to the right
+				nextLetter := int(m[ri][ci+1])  // letter in that cell
+				if nextLetter-thisLetter <= 1 { // if going up at most 1,
+					g.AddCost(thisNode, nextNode, 1) // can go right
 				}
-				if thisLetter-nextLetter <= maxDiff { // left
+				if thisLetter-nextLetter <= 1 { // same for go left
 					g.AddCost(nextNode, thisNode, 1)
 				}
 			}
@@ -74,21 +73,22 @@ func main() {
 			if ri < nr-1 {
 				nextNode := (ri+1)*nc + ci
 				nextLetter := int(m[ri+1][ci])
-				if nextLetter-thisLetter <= maxDiff { // go down
+				if nextLetter-thisLetter <= 1 { // go down
 					g.AddCost(thisNode, nextNode, 1)
 				}
-				if thisLetter-nextLetter <= maxDiff { // up
+				if thisLetter-nextLetter <= 1 { // up
 					g.AddCost(nextNode, thisNode, 1)
 				}
 			}
 		}
 	}
 
-	// Part 1: calculate shortest path from S to E
+	// Part 1: calculate shortest feasible path from S to E
 	_, dist := graph.ShortestPath(g, S, E)
 	fmt.Println("Part 1 (s/b 31, 490):", dist)
 
-	// Part 2: find the shortest path from any 'a' cell to E
+	// Part 2: find the shortest feasible path from any 'a' cell to E
+	// (note that ShortestPath returns -1 if no path found)
 	var shortest int64
 	for ri := 0; ri < nr; ri++ {
 		for ci := 0; ci < nc; ci++ {
