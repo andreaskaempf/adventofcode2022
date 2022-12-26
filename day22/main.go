df58925adbad222688d1ee2a1ba9cfae86f2aa5b
Andreas Kaempf <andreas.kaempf@soundience.com>
2022-12-26 14:06:09 +0100

    Refactored to prepare for Part 2

--- day22/main.go
+++ day22/main.go
@@ -19,65 +19,77 @@ type Point struct {
 	x, y int
 }
 
+// Keep track of what is at each location
 var tiles map[Point]byte
 
+// Keep track of the minimum X and Y values for each row/col
 var minX, maxX, minY, maxY map[int]int
+
+// List of instructions, left and right encoded as -1 and -2
+var instructions []int
 
 const LEFT int = -1
 const RIGHT int = -2
 
 func main() {
 
-	// Read the input file: map until blank line, then set of
-	// instructions
+	// Read the input file: map until blank line, then set of instructions
 	fname := "sample.txt"
-	//fname = "input.txt"
-	readingMap := true
-	tiles = map[Point]byte{}
-	var x, y int
-	instructions := []int{}
-	for _, l := range readLines(fname) {
-		if len(l) == 0 {
-			readingMap = false
-		} else if readingMap {
-			for x = 0; x < len(l); x++ {
-				if l[x] != ' ' {
-					tiles[Point{x + 1, y + 1}] = l[x]
-				}
-			}
-			y++
-		} else {
-			instructions = parseInstructions(l)
-		}
-	}
+	fname = "input.txt"
+	readMap(fname)
 
-	// Get the min/max col in each row and min/max row in each column
-	minX = map[int]int{}
-	maxX = map[int]int{}
-	minY = map[int]int{}
-	maxY = map[int]int{}
-	for p, _ := range tiles {
-		if minX[p.y] == 0 || p.x < minX[p.y] {
-			minX[p.y] = p.x
-		}
-		if p.x > maxX[p.y] {
-			maxX[p.y] = p.x
-		}
-		if minY[p.x] == 0 || p.y < minY[p.x] {
-			minY[p.x] = p.y
-		}
-		if p.y > maxY[p.x] {
-			maxY[p.x] = p.y
-		}
-	}
+	// Do part 1
+	part1()
 
-	// Start in the first open tile on the first row
-	y = 1
-	x = minX[1]
+	// Do part 2
+	part2()
+}
+
+// Part 2: same as part 1, but wrap around cube instead of 2-d space
+func part2() {
+
+	// Start in the first open tile on the first row, facing right
+	y := 1
+	x := minX[1]
 	assert(tiles[Point{x, y}] == '.', "First tile is not open!")
+	dir := 90 // start facing right
 
 	// Process instructions, simplify by adding up all moves in each direction?
+	for _, inst := range instructions {
+
+		if inst == RIGHT { // Rotate right
+			dir = ifElse(dir == 270, 0, dir+90)
+		} else if inst == LEFT { // Rotate left
+			dir = ifElse(dir == 0, 270, dir-90)
+		} else if dir == 90 { // Move right
+			x1 := x
+			y1 := y
+			for k := 0; k < inst; k++ {
+				x1++
+				if tiles[Point{x1, y1}] == 0 { // no tile, wrap to beginning
+					x1 = minX[y1]
+				}
+				if tiles[Point{x1, y1}] == '.' {
+					x = x1
+				} else {
+					break
+				}
+			}
+		}
+	}
+
+}
+
+// Part 1: follow instructions, moving around 2-d space, wrapping as necessary
+func part1() {
+
+	// Start in the first open tile on the first row, facing right
+	y := 1
+	x := minX[1]
+	assert(tiles[Point{x, y}] == '.', "First tile is not open!")
 	dir := 90 // start facing right
+
+	// Process instructions, simplify by adding up all moves in each direction?
 	for i := 0; i < len(instructions); i++ {
 
 		// Process a turn
@@ -150,14 +162,57 @@ func main() {
 		}
 
 	}
+
 	//fmt.Printf("Final x = %d, y = %d, dir = %d (s/b 8, 6, dir 90)\n", x, y, dir)
 	facing := map[int]int{0: 3, 90: 0, 180: 1, 270: 2}
 	score := 1000*y + 4*x + facing[dir]
 	fmt.Println("Part 1: score (s/b 6032, 36518) =", score)
 }
 
-// Move in current direction, given number of steps
-//func move()
+// Read the input file: map until blank line, then set of instructions,
+// also set min/max X/Y
+func readMap(fname string) {
+
+	// Read the input file: map until blank line, then set of instructions
+	readingMap := true
+	tiles = map[Point]byte{}
+	var x, y int
+	instructions = []int{}
+	for _, l := range readLines(fname) {
+		if len(l) == 0 {
+			readingMap = false
+		} else if readingMap {
+			for x = 0; x < len(l); x++ {
+				if l[x] != ' ' {
+					tiles[Point{x + 1, y + 1}] = l[x]
+				}
+			}
+			y++
+		} else {
+			instructions = parseInstructions(l)
+		}
+	}
+
+	// Get the min/max col in each row and min/max row in each column
+	minX = map[int]int{}
+	maxX = map[int]int{}
+	minY = map[int]int{}
+	maxY = map[int]int{}
+	for p, _ := range tiles {
+		if minX[p.y] == 0 || p.x < minX[p.y] {
+			minX[p.y] = p.x
+		}
+		if p.x > maxX[p.y] {
+			maxX[p.y] = p.x
+		}
+		if minY[p.x] == 0 || p.y < minY[p.x] {
+			minY[p.x] = p.y
+		}
+		if p.y > maxY[p.x] {
+			maxY[p.x] = p.y
+		}
+	}
+}
 
 // Parse string of instructions: list of numbers,
 // where -1 means left and -2 means right
